fdedf111c01ea68fa8f126d5426b82afb31971af
Clerisson Lins <clerissonlins797@gmail.com>
2023-04-11 15:33:53 -0300

    VideoCallGambs

--- src/Controllers/VideoCallController.js
+++ src/Controllers/VideoCallController.js
@@ -11,9 +11,11 @@ const { api_channels } = require("../Models/index");
 const appId = "51789cde1f9047c6b96a865b5bf6921a";
 const appCertificate = "ca840f0d5fe8405d94ec083876cd6ca4";
 const role = RtcRole.PUBLISHER;
-const expirationTimeInSeconds = 133600;
+const expirationTimeInSeconds = 42220;
 const currentTimestamp = Math.floor(Date.now() / 1000);
 const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
+
+const newDate = new Date();
 
 class VideoCallController {
   static getVideoToken = async (req, res) => {
@@ -21,6 +23,47 @@ class VideoCallController {
     const uid = req.body.id;
 
     let channelName;
+    let initialHour;
+
+    function checkTwoUsers(obj) {
+      return obj.usersOnline === 2;
+    }
+    function checkOneUser(obj) {
+      return obj.usersOnline === 1;
+    }
+    function checkZeroUsers(obj) {
+      return obj.usersOnline === 0;
+    }
+
+    async function updateChannel(id) {
+      api_channels.update(
+        { usersOnline: 0, status: "offline", startHour: null },
+        {
+          where: {
+            id: id,
+          },
+        }
+      );
+    }
+
+    async function updtadeAfkChannel(id) {
+      api_channels
+        .findOne({
+          where: {
+            id: id,
+          },
+        })
+        .then((data) => {
+          if (data.dataValues.usersOnline === 1) {
+            api_channels.update(
+              { usersOnline: 0, status: "offline" },
+              { where: { id: id } }
+            );
+          } else {
+            return;
+          }
+        });
+    }
 
     try {
       await api_channels
@@ -31,16 +74,6 @@ class VideoCallController {
           },
         })
         .then((data) => {
-          function checkTwoUsers(obj) {
-            return obj.usersOnline === 2;
-          }
-          function checkOneUser(obj) {
-            return obj.usersOnline === 1;
-          }
-          function checkZeroUsers(obj) {
-            return obj.usersOnline === 0;
-          }
-
           const channelWithTwoUsers = data.find(checkTwoUsers);
           if (channelWithTwoUsers !== undefined) {
             api_channels.update(
@@ -48,32 +81,31 @@ class VideoCallController {
               { where: { id: channelWithTwoUsers.dataValues.id } }
             );
             channelName = channelWithTwoUsers.dataValues.name;
+            initialHour = channelWithTwoUsers.dataValues.initialHour;
           } else {
             const channelWithOneUser = data.find(checkOneUser);
             if (channelWithOneUser !== undefined) {
               api_channels.update(
-                { usersOnline: 2 },
+                { usersOnline: 2, startHour: newDate },
                 { where: { id: channelWithOneUser.dataValues.id } }
               );
               channelName = channelWithOneUser.dataValues.name;
+              initialHour = newDate;
               setTimeout(() => {
-                api_channels.update(
-                  { usersOnline: 0, status: "offline", minStart: null },
-                  {
-                    where: {
-                      id: channelWithOneUser.dataValues.id,
-                    },
-                  }
-                );
-              }, 420000);
+                updateChannel(channelWithOneUser.dataValues.id);
+              }, 1200000);
             } else {
               const channelWithZeroUsers = data.find(checkZeroUsers);
               if (channelWithZeroUsers !== undefined) {
                 channelName = channelWithZeroUsers.dataValues.name;
+                initialHour = newDate;
                 api_channels.update(
-                  { usersOnline: 1 },
+                  { usersOnline: 1, startHour: newDate },
                   { where: { id: channelWithZeroUsers.dataValues.id } }
                 );
+                setTimeout(() => {
+                  updtadeAfkChannel(channelWithZeroUsers.dataValues.id);
+                }, 180000);
               } else {
                 return res.status(200).send({
                   status: false,
@@ -83,28 +115,16 @@ class VideoCallController {
             }
           }
 
-          // const tokenA = RtcTokenBuilder.buildTokenWithUid(
-          //   appId,
-          //   appCertificate,
-          //   channelName,
-          //   uid,
-          //   role,
-          //   privilegeExpiredTs
-          // );
-
           res.status(200).send({
             channelName: channelName,
             // token: tokenA,
+            initialHour: initialHour,
+            now: newDate,
           });
         })
         .catch((err) => {
           console.log(err);
         });
-
-      // console.log("Token with integer number Uid: " + tokenA);
-
-      // res.status(200)
-      // .send(tokenA);
     } catch (error) {
       console.log(error);
     }
@@ -125,6 +145,9 @@ class VideoCallController {
           if (data.dataValues.usersOnline > 0) {
             res.status(200).send({
               status: true,
+              user: data.dataValues.usersOnline,
+              initialHour: data.dataValues.initialHour,
+              now: newDate,
             });
           } else {
             res.status(200).send({
@@ -133,7 +156,7 @@ class VideoCallController {
           }
         } else {
           res.status(200).send({
-            status: "inexistente",
+            status: false,
           });
         }
       })
