4f3f57c3667c6681f8d0130548066e23698df00d
Clerisson Lins <clerissonlins797@gmail.com>
2023-04-21 23:47:02 -0300

    changes

--- serve.js
+++ serve.js
@@ -15,13 +15,12 @@ const minutes = newDate.getMinutes();
 let rooms = {};
 
 wss.on("connection", function connection(ws, req) {
-  if (hours !== 9 || minutes > 20) {
-    ws.close();
-  }
+  // if (hours !== 9 || minutes > 20) {
+  //   ws.close();
+  // }
 
   // client conectado com o websocket
-  const parameters = url.parse(req.url, true);
-  ws.uid = parameters.query.myCustomID;
+
   //id recebido do client pela url
 
   ws.on("message", function message(data) {
@@ -31,27 +30,42 @@ wss.on("connection", function connection(ws, req) {
 
     switch (type) {
       case "connect":
+        saveId(obj.id);
         createOrJoin();
         break;
 
+      case "cancel":
+        leave();
+        break;
       default:
         console.warn(`Type: ${type} unknown`);
         break;
     }
   });
 
+  function saveId(id) {
+    if (ws.uid) {
+      return;
+    }
+    ws.uid = id;
+    console.log("id saved", id);
+  }
+
   function createOrJoin() {
     //verifica se não existe nenhuma room e então cria uma.
+    console.log(" algum user entrou aqui");
     const keys = Object.keys(rooms);
     const length = keys.length;
+    console.log(length);
+
     if (length === 0) {
       return create();
     }
 
     // Verifica se o usuario já estava conectado em alguma room pelo id.
     const check = checkReconnect(keys, length);
-    if (check === true) {
-      return joinReconnect(target.room);
+    if (check.status === true) {
+      return joinReconnect(check.room);
     }
 
     // verifica se existe alguma sala com vagas disponiveis.
@@ -65,32 +79,57 @@ wss.on("connection", function connection(ws, req) {
     return create();
   }
 
+  function send(obj) {
+    if (ws.OPEN) {
+      const stringify = JSON.stringify(obj);
+      console.log(stringify);
+      return ws.send(stringify);
+    } else {
+      console.log("nenhuma conexão");
+    }
+  }
+
   function create() {
+    console.log("entrou create");
+    //cria uma sala com um nome gerado aleatoriamente
     const room = genKey(8);
     rooms[room] = [ws];
     ws["room"] = room;
 
+    // envia um objeto com o status para mudar a section no front
     const obj = {
-      status: "WAITING_ALONE",
+      status: "WAITING_MORE_USERS",
+      usersCount: 1,
     };
 
     ws.send(JSON.stringify(obj));
   }
 
   function checkReconnect(keys, length) {
+    console.log("reconect");
+    // checa se o id do user já tinha sido atribuida a alguma sala e reconecta
     for (let i = 0; i < length; i++) {
       let namesRooms = rooms[keys[i]];
-      for (let a = 0; a < length; a++) {
-        let target = namesRooms[a];
-        if (target.uid === ws.uid) {
-          return true;
+      if (namesRooms.length !== 0) {
+        for (let a = 0; a < length; a++) {
+          let target = namesRooms[a];
+          // condição necessario pois a função de leave / cancel deixa uma propriedade com array vazio
+          if (target.uid === ws.uid) {
+            return {
+              status: true,
+              room: target.room,
+            };
+          }
         }
       }
     }
-    return false;
+    return {
+      status: false,
+    };
   }
 
   function joinReconnect(roomName) {
+    // reconecta em uma sala já existente, não valida o tamanho maximo
     const room = roomName;
     if (!Object.keys(rooms).includes(room)) {
       console.warn(`Room ${room} does not exist!`);
@@ -98,16 +137,20 @@ wss.on("connection", function connection(ws, req) {
       return;
     }
 
+    // retorna call start para iniciar a video chamada no front
+
     rooms[room].push(ws);
     ws["room"] = room;
     const obj = {
-      status: "CALL_START",
-      channelName: roomName,
+      status: "CALL_START_RECONNECT",
+      channel: roomName,
     };
-    ws.send(JSON.stringify(obj));
+    send(obj);
   }
 
   function join(roomName) {
+    console.log("join");
+    // entra em uma sala caso não esteja cheia
     const room = roomName;
     let obj;
     if (!Object.keys(rooms).includes(room)) {
@@ -123,11 +166,13 @@ wss.on("connection", function connection(ws, req) {
     }
 
     if (rooms[room].length >= 2) {
+      // se for a ultima pessoa a entrar emite o call start para todos os integrantes da room
       rooms[room].push(ws);
       ws["room"] = room;
       obj = {
         status: "CALL_START",
-        channelName: roomName,
+        usersCount: 3,
+        channel: roomName,
       };
       return wss.clients.forEach(function each(client) {
         if (client.readyState === WebSocket.OPEN && ws["room"] == roomName) {
@@ -136,27 +181,49 @@ wss.on("connection", function connection(ws, req) {
       });
     }
 
-    rooms[room].push(ws);
-    ws["room"] = room;
-    obj = {
-      status: "WAITING_WITH_USER",
-    };
-    return wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN && ws["room"] == roomName) {
-        client.send(JSON.stringify(obj));
-      }
-    });
+    // se for a segunda pessoa a entrar, emite uma função para ele e o outro user,
+    // feedback que uma segunda pessoa foi encontrada.
+    if (rooms[room].length > 0) {
+      rooms[room].push(ws);
+      ws["room"] = room;
+      obj = {
+        status: "WAITING_MORE_USERS",
+        usersCount: 2,
+      };
+      return wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN && ws["room"] == roomName) {
+          client.send(JSON.stringify(obj));
+        }
+      });
+    }
+    if (rooms[room].length === 0) {
+      rooms[room].push(ws);
+      ws["room"] = room;
+      obj = {
+        status: "WAITING_MORE_USERS",
+        usersCount: 1,
+      };
+      send(obj);
+    }
   }
 
-  // function leave(params) {
-  //   const room = ws.room;
-  //   rooms[room] = rooms[room].filter((so) => so !== ws);
-  //   ws["room"] = undefined;
+  function leave() {
+    if (ws.room) {
+      const room = ws.room;
+      rooms[room] = rooms[room].filter((so) => so !== ws);
+      ws["room"] = undefined;
+      console.log("leave");
+    }
+  }
 
-  //   if (rooms[room].length == 0) close(room);
-  // }
   ws.on("close", () => {
-    console.log("Conexão encerrada");
+    if (ws) {
+      ws.close();
+      console.log("Conexão encerrada");
+    }
+  });
+  ws.on("error", (e) => {
+    console.log(e);
   });
 });
 
